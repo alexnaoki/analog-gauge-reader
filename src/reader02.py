925a75df854472e123aa994b8c372db54ed714ad
Alex Kobayashi <alexkobayashi10@gmail.com>
2020-07-13 13:17:43 -0300

    .

--- src/reader02.py
+++ src/reader02.py
@@ -3,9 +3,10 @@ import numpy as np
 
 class analog_gauge:
     def __init__(self, file):
+        self.file = file
         self.video = cv.VideoCapture(file)
-        fps = self.video.get(cv.CAP_PROP_FPS)
-        print('FPS:',fps)
+        self.fps = self.video.get(cv.CAP_PROP_FPS)
+        print('FPS:', self.fps)
 
         while True:
             _, self.image = self.video.read()
@@ -27,8 +28,64 @@ class analog_gauge:
         cv.waitKey(-1)
         cv.destroyAllWindows()
 
-    def head_teste(self):
-        
+    def head_test(self):
+        self.rect_pts_BEFORE_np = np.float32(self.rect_pts_BEFORE)
+        print(self.rect_pts_BEFORE)
+        n = 10
+        count = 0
+
+        self.video_test = cv.VideoCapture(self.file)
+        # success, image = self.video_test.read()
+        while n>0:
+            success, image = self.video_test.read()
+
+            print(n)
+            image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+            try:
+                matrix = cv.getPerspectiveTransform(self.rect_pts_BEFORE_np, self.rect_pts_AFTER)
+                result = cv.warpPerspective(image_gray, matrix, (135, 135))
+
+                width, height = result.shape[:2]
+
+                mask = np.zeros_like(result)
+                mask = cv.circle(mask, center=(int(height/2), int(width/2)), radius=200, color=(255,255,255), thickness=-1)
+                clock = np.bitwise_and(result, mask)
+
+                clock_eq = cv.equalizeHist(clock)
+
+                min_treshold = 100
+                max_treshold = 230
+                _, clock_t = cv.threshold(clock_eq, min_treshold, max_treshold, cv.THRESH_BINARY_INV)
+
+                clock_c, _ = cv.findContours(clock_t, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+                for contour in clock_c:
+                    area = cv.contourArea(contour)
+                    print(area)
+                    M = cv.moments(contour)
+                    teste = []
+                    # print(area)
+
+                    # if area > 10000:
+                    #     cx = int(M['m10']/M['m00'])
+                    #     cy = int(M['m01']/M['m00'])
+                    #     cv.circle(clock_t, (cx, cy), 0, (0,255,0), 5)
+                    #
+                    # if (area<10000) and (area>2500):
+                    #     cx = int(M['m10']/M['m00'])
+                    #     cy = int(M['m01']/M['m00'])
+                    #     cv.circle(clock_t, (cx, cy), 0, (0,255,0), 5)
+            except:
+                pass
+
+            seconds = 90
+            count += self.fps * seconds
+            print(count)
+            self.video_test.set(1, count)
+            break
+            n -= 1
+
 
         # try:
         #     self.rect_pts_BEFORE_np = np.float32(self.rect_pts_BEFORE)
@@ -83,9 +140,20 @@ class analog_gauge:
 
             cv.imshow('imag', self.image_gray)
 
-    # def
-
+    def most_distance_pts_from_contour(self, centroid, pts):
+        fartest = 0
+        for i, point in enumerate(pts):
+            dist = ((centroid[0] - point[0])**2 + (centroid[1] - point[1])**2)**(1/2)
+            # print(point, dist)
+            if i == 0:
+                fartest = dist
+                index = i
+            elif fartest < dist:
+                fartest = dist
+                index = i
+        return index, fartest
 
 file = r'G:\Meu Drive\Colab Notebooks\opencv_data\ch01_20181210175338.mp4'
 a = analog_gauge(file=file)
 a.locate_gauge()
+a.head_test()
